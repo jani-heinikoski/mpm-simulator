eaa2f129fbc377844f0a0e27152555ff621ed6d9
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2023-02-10 18:29:54 +0200

    removed strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -8,9 +8,7 @@ import "./css/superhero.bootswatch.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
