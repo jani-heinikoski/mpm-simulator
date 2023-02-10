243c9aeff976888756dd99a608de8b6cef7c6249
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2023-02-10 18:25:48 +0200

    added table of references

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
 
 import Card from "./Card";
 import HomePageHero from "./HomePageHero";
@@ -23,7 +24,7 @@ const HomePage = () => {
             header="Microprogramming"
             border="danger"
             title="What is microprogramming?"
-            text="Microprogramming was invented by Maurice Wilkes in 1951 to simplify circuitry in computer systems. It is the act of creating microprograms which consist of very primitive microinstructions. Microprogramming is typically used to implement machine instructions instead of implementing them with complex circuitry. It also enables engineers to implement a machine’s instruction set differently with varying efficiencies and costs."
+            text="Microprogramming was invented by Maurice Wilkes in 1951 to simplify circuitry in computer systems. It is the act of creating microprograms which consist of very primitive microinstructions. Microprogramming is typically used to implement machine instructions instead of implementing them with complex circuitry. It also enables engineers to implement a machine’s instruction set differently with varying efficiencies and costs. [2], [3]"
         />
     );
 
@@ -36,6 +37,78 @@ const HomePage = () => {
             <HorizontalDivider />
             <Row>
                 <Col md={{ span: 6, offset: 6 }}>{microprogrammingCard}</Col>
+            </Row>
+            <HorizontalDivider />
+            <Row>
+                <Col className="p-4">
+                    <h3 className="fw-bold">Table of references</h3>
+                    <Table
+                        striped
+                        bordered
+                        hover
+                        variant="dark"
+                        className="fs-6"
+                    >
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Source</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>
+                                    J. Boberg, ‘Jorma Boberg: Materiaaleja’,
+                                    Sep. 29, 2016.{" "}
+                                    <span className="fst-italic">
+                                        <a
+                                            href="http://staff.cs.utu.fi/staff/jorma.boberg/Mat/"
+                                            className="link-primary"
+                                        >
+                                            http://staff.cs.utu.fi/staff/jorma.boberg/Mat/
+                                        </a>
+                                    </span>{" "}
+                                    (accessed Jun. 02, 2022).
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>
+                                    Association for Computing Machinery (acm),
+                                    ‘Maurice V. Wilkes Additional Materials’,
+                                    2019.{" "}
+                                    <span className="fst-italic">
+                                        <a
+                                            href="https://amturing.acm.org/info/wilkes_1001395.cfm"
+                                            className="link-primary"
+                                        >
+                                            https://amturing.acm.org/info/wilkes_1001395.cfm
+                                        </a>
+                                    </span>{" "}
+                                    (accessed Jun. 22, 2022).
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>
+                                    Britannica, The Editors of Encyclopaedia,
+                                    ‘microprogramming | Definition & Facts |
+                                    Britannica’, Sep. 14, 2021.{" "}
+                                    <span className="fst-italic">
+                                        <a
+                                            href="https://www.britannica.com/technology/microprogramming"
+                                            className="link-primary"
+                                        >
+                                            https://www.britannica.com/technology/microprogramming
+                                        </a>
+                                    </span>{" "}
+                                    (accessed Jun. 22, 2022).
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Col>
             </Row>
         </Container>
     );
