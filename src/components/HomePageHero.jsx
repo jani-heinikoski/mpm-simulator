68f2ee68fe734bc86d50da802fbd19d4124d01f7
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2023-02-10 18:26:04 +0200

    added references

--- src/components/HomePageHero.jsx
+++ src/components/HomePageHero.jsx
@@ -8,49 +8,51 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
 const HomePageHero = () => {
-  const navigate = useNavigate();
-  return (
-    <Row className="bg-dark p-4">
-      <Col className="py-5">
-        <h1 className="fw-bold text-center text-white">
-          Welcome to the <Badge bg="danger">MPM Simulator</Badge> home page
-        </h1>
-        <Row>
-          <Col md={12} lg={8} className="mx-auto">
-            <p className="fs-5 mb-4">
-              Learn microprogramming concepts with this interactive simulator
-              app. The simulator is based on the abstract microprogrammed
-              machine introduced in{" "}
-              <span className="fst-italic">
-                <a
-                  href="http://staff.cs.utu.fi/staff/jorma.boberg/Mat/"
-                  className="link-primary"
-                >
-                  Johdatus tietojenkäsittelytieteeseen
-                </a>
-              </span>{" "}
-              by Jorma Boberg, Martti Penttonen, Tapio Salakoski, Jukka Teuhola
-              and other members of University of Turku.
-            </p>
-          </Col>
+    const navigate = useNavigate();
+    return (
+        <Row className="bg-dark p-4">
+            <Col className="py-5">
+                <h1 className="fw-bold text-center text-white">
+                    Welcome to the <Badge bg="danger">MPM Simulator</Badge> home
+                    page
+                </h1>
+                <Row>
+                    <Col md={12} lg={8} className="mx-auto">
+                        <p className="fs-5 mb-4">
+                            Learn microprogramming concepts with this
+                            interactive simulator app. The simulator is based on
+                            the abstract microprogrammed machine introduced in{" "}
+                            <span className="fst-italic">
+                                <a
+                                    href="http://staff.cs.utu.fi/staff/jorma.boberg/Mat/"
+                                    className="link-primary"
+                                >
+                                    Johdatus tietojenkäsittelytieteeseen
+                                </a>
+                            </span>{" "}
+                            by Jorma Boberg, Martti Penttonen, Tapio Salakoski,
+                            Jukka Teuhola and other members of University of
+                            Turku [1].
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col xs="auto">
+                        <Button
+                            variant="outline-danger"
+                            size="lg"
+                            className="fw-bold"
+                            onClick={() => {
+                                navigate("/simulator");
+                            }}
+                        >
+                            Take me to the simulator
+                        </Button>
+                    </Col>
+                </Row>
+            </Col>
         </Row>
-        <Row className="justify-content-center">
-          <Col xs="auto">
-            <Button
-              variant="outline-danger"
-              size="lg"
-              className="fw-bold"
-              onClick={() => {
-                navigate("/simulator");
-              }}
-            >
-              Take me to the simulator
-            </Button>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  );
+    );
 };
 
 export default HomePageHero;
