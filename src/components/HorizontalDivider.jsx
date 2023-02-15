a78718c9283a66a0baddb528abffedd4bfaaa640
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2023-02-15 17:41:35 +0200

    HorizontalDivider forwards ref to actual hr

--- src/components/HorizontalDivider.jsx
+++ src/components/HorizontalDivider.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const HorizontalDivider = () => {
-    return <hr className="my-4 text-dark opacity-100" />;
-};
+const HorizontalDivider = React.forwardRef((_, ref) => {
+    return <hr ref={ref} className="my-4 text-dark opacity-100" />;
+});
 
 export default HorizontalDivider;
